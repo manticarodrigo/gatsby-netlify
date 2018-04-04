02a4bc69302f4f5befb1d95c37993cba267fa5ac
Marcos Moreno <marcosmorenomamg@gmail.com>
2018-04-04 11:02:46 -0600

    changed site meta data

---
+++ meta/config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by vaibhav on 31/3/18
+ */
+module.exports = {
+    siteTitle: "Gatsby Netlify", // Site title.
+    siteTitleAlt: "Business Blog", // Alternative site title for SEO.
+    siteLogo: "/logos/logo-512x512.png", // Logo used for SEO and manifest.
+    siteUrl: "https://gatsby-ooqia.netlify.com", // Domain of your website without pathPrefix.
+    pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+    siteDescription: "Leverage Gatsby Business Blog for your Business.", // Website description used for RSS feeds/meta description tag.
+    siteRss: "/rss.xml",
+    siteFBAppID: "", // FB Application ID for using app insights
+    googleTagManagerID: "", // GTM tracking ID.
+    userName: "Rodrigo Mantica",
+    userTwitter: "manticarodrigo",
+    userLocation: "Managua, Nicaragua",
+    userDescription: "",
+    copyright: "Copyright © Gatsby Netlify 2018. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+    themeColor: "#00d1b2", // Used for setting manifest and progress theme colors.
+    backgroundColor: "#ffffff" // Used for setting manifest background color.
+};
